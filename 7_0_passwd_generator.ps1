2df9cf1fb19819d4d1c3ce529ab7db9ce3c99ab6
AngryW0lf <koslovdennis@gmail.com>
2021-12-06 22:36:11 +0200

    Updating password generator script

--- 7_0_passwd_generator.ps1
+++ 7_0_passwd_generator.ps1
@@ -1,28 +1,30 @@
 function CreatePassword (){
+    
     param (
         $Chars = "!@#$%^&*0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz",
         $Min = 8,
         $Max = 12
     )
-#Choosing the random characters number in this password:
-$MaxNumber = Get-Random -Minimum $Min -Maximum $Max
 
-#Showing the maximum number of characters (plus 1 character provided by zero)
-Write-Host "Max Number of Characters for this password is: " -ForegroundColor Yellow -NoNewline
-Write-Host ($MaxNumber+1)
+    #Choosing the random characters number in this password:
+    $MaxNumber = Get-Random -Minimum $Min -Maximum $Max
 
-#Create random password
-$Password = New-Object -TypeName PSObject
-$Password | Add-Member `
+    #Showing the maximum number of characters (plus 1 character provided by zero)
+    Write-Host "Max Number of Characters for this password is: " -ForegroundColor Yellow -NoNewline
+    Write-Host ($MaxNumber+1)
+
+    #Create random password
+    $Password = New-Object -TypeName PSObject
+    $Password | Add-Member `
     -MemberType ScriptProperty `
     -Name "Password" `
     -Value {
         ($Chars.tochararray() | Sort-Object {Get-Random})[0..$MaxNumber] -join ''
     }
-Write-Host "The password is: " -ForegroundColor Green -NoNewline
-Write-Host ($Password | Select-Object -ExpandProperty "Password")
-Write-Host "The password length is: " -ForegroundColor Green -NoNewline
-Write-Host ($Password | Select-Object -ExpandProperty "Password").Length
+    Write-Host "The password is: " -ForegroundColor Green -NoNewline
+    Write-Host ($Password | Select-Object -ExpandProperty "Password")
+    Write-Host "The password length is: " -ForegroundColor Green -NoNewline
+    Write-Host ($Password | Select-Object -ExpandProperty "Password").Length
 }
 
 
