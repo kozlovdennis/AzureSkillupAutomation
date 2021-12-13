afcbba7c91611a8d2257104b1b4f0c8359fe159c
AngryW0lf <koslovdennis@gmail.com>
2021-12-13 10:53:02 +0200

    Updating scripts

--- 6_3_sa_generate_sas.ps1
+++ 6_3_sa_generate_sas.ps1
@@ -1,9 +1,21 @@
+param (
+    $Location = 'North Europe',
+    $RG = 'SKILLUP-RG',
+    $ServiceSA = "sasrvsunmonster",
+    $Container = "files",
+    $Path2File = "C:\Users\kozlov.d\Downloads\ChromeStandaloneSetup64.exe"
+)
 
+Connect-AzAccount
+
+$UploadStorage=Get-AzStorageAccount -ResourceGroupName $RG -Name $ServiceSA
+$SAContext=$UploadStorage.Context
 
 New-AzStorageAccountSASToken `
     -Service 'Blob','File','Table','Queue' `
     -ResourceType 'Service','Container','Object' `
-    -Permission "racwdlup"
-    -Protocol 'HttpsOnly'
+    -Permission "racwdlup" `
+    -Protocol 'HttpsOnly' `
+    -Context $SAContext
 
 
